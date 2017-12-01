5abd96abe55fbd378f938ad4fbe5c2edfd8ee915
Edward Vidal <develone@sbcglobal.net>
2017-12-01 15:49:08 +0000

    scripts to run on touch display and needed deb pkgs for keyboard

---
+++ Desktop/teapot.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+cd ~/userland_pre/host_applications/linux/apps/hello_pi/hello_teapot
+sudo ./hello_teapot.bin ; sleep 5;exit
+
+
---
+++ Desktop/tiger.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+cd ~/userland_pre/host_applications/linux/apps/hello_pi/hello_tiger
+sudo ./hello_tiger.bin ; sleep 5;exit
+
+
